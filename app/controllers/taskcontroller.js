6067b2a73049480d30a22bedc982dbb76db021f0
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-10-20 03:36:55 +0600

    APP File Update

---
+++ app/controllers/taskcontroller.js
@@ -0,0 +1,23 @@
+//create
+
+export const createTask= async (req,res)=>{
+    return res.json({message:'Task created successfully!'})
+}
+
+//read
+
+export const readTask = async (req,res)=>{
+    return res.json({message:'Task read successfully!'})
+}
+
+//create
+
+export const updateTask = async (req,res)=>{
+    return res.json({message:'Task update successfully!'})
+}
+
+//create
+
+export const deleteTask = async (req,res)=>{
+    return res.json({message:'Task delete successfully!'})
+}
