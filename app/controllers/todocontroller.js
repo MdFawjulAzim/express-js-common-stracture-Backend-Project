5b2518a53dd521c02445572caa34a41780f1a6af
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-10-20 03:28:00 +0600

    controller file add

---
+++ app/controllers/todocontroller.js
@@ -0,0 +1,23 @@
+//create
+
+export const createTodo = async (req,res)=>{
+    return res.json({message:'Todo created successfully!'})
+}
+
+//read
+
+export const readTodo = async (req,res)=>{
+    return res.json({message:'Todo read successfully!'})
+}
+
+//create
+
+export const updateTodo = async (req,res)=>{
+    return res.json({message:'Todo update successfully!'})
+}
+
+//create
+
+export const deleteTodo = async (req,res)=>{
+    return res.json({message:'Todo delete successfully!'})
+}
